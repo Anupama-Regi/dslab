6c71a6b77814b2abee16d2cd1bc97e5f802a1bea
Anupama-Regi <74719646+Anupama-Regi@users.noreply.github.com>
2020-12-11 17:47:03 +0530

    Add files via upload

---
+++ 2-arraydelete.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+int a[100],n,i,p;
+void delete();
+void display();
+void main()
+{
+int c;
+printf("\n Enter the array size : ");
+scanf("%d",&n);
+printf("\nEnter the elements one by one : ");
+for(i=0;i<n;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+printf("\nThe array elements are : ");
+for(i=0;i<n;i++)
+	{
+	printf("%d\t",a[i]);
+	}
+printf("\n1.DELETE");
+printf("\n2.DISPLAY");
+printf("\n3.EXIT");
+do
+{
+printf("\n\nEnter your choice : ");
+scanf("%d",&c);
+switch(c)
+{
+case 1:
+{
+delete();
+break;
+}
+case 2:
+{
+display();
+break;
+}
+case 3:return;
+default:printf("\nYou entered invalid option");
+}
+}
+while(c!=3);
+}
+
+void delete()
+{
+if(n==0)
+{
+printf("Array is empty..");
+}
+else{
+printf("\nEnter the position you want to delete : ");
+scanf("%d",&p);
+if (p>=n+1)
+printf("Deletion cannot be performed......");
+else
+{
+for(i=p-1;i<n-1;i++)	
+a[i]=a[i+1];
+}
+n--;
+}
+}
+
+void display()
+{
+if(n==0)
+{
+printf("Array is empty..");
+}
+else
+{
+printf("The array is : ");
+for(i=0;i<=n-1;i++)
+printf("%d\t",a[i]);
+}
+}
